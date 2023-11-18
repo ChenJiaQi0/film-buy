fbd66baa5ddbf605b5f6e82668cc570c71a78899
TONGTONGDESHIJIE <1090060206@qq.com>
2023-11-18 20:30:33 +0800

    影院模块：根据条件获取影院列表

--- filmbuy-app/cinema/src/main/java/top/chen/cinema/controller/CinemaController.java
+++ filmbuy-app/cinema/src/main/java/top/chen/cinema/controller/CinemaController.java
@@ -1,6 +1,7 @@
 package top.chen.cinema.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 import top.chen.cinema.domain.entity.Cinema;
 import top.chen.cinema.service.CinemaService;
 import top.chen.common.result.Result;
+
+import java.util.List;
 
 /**
  * <p>
@@ -23,5 +26,27 @@ import top.chen.common.result.Result;
 @RestController
 @RequestMapping("/cinema")
 public class CinemaController {
+    @Resource
+    private CinemaService cinemaService;
+
+    /**
+     * 根据条件获取影院列表
+     * @param province
+     * @param city
+     * @param area
+     * @return
+     */
+    @GetMapping()
+    public Result<List<Cinema>> list(@RequestParam(required = false) String province,
+                                     @RequestParam(required = false) String city,
+                                     @RequestParam(required = false) String area) {
+        LambdaQueryWrapper<Cinema> wp = new LambdaQueryWrapper<>();
+        Result<List<Cinema>> resp = new Result<>();
+        if (province != null) wp.like(Cinema::getProvince, province);
+        if (city != null) wp.like(Cinema::getCity, city);
+        if (area != null) wp.like(Cinema::getArea, area);
+        resp.setData(cinemaService.list(wp));
+        return resp;
+    }
 }
 
