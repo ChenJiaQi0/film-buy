b3a0ccc1590e39ad1ee80f0f15dfe53f90e140f2
TONGTONGDESHIJIE <1090060206@qq.com>
2023-12-05 14:44:33 +0800

    实现网关注册 nacos

---
+++ filmbuy-app/gateway/src/main/java/top/chen/gateway/GatewayApplication.java
@@ -0,0 +1,16 @@
+package top.chen.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ChenQi
+ * @date 2023/12/4
+ * @description: GatewayApplication
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
